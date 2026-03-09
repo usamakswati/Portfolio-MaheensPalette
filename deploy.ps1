d900e83c8e1f98b608a8a7955623549e1de6c152
Maheen Nouman <imaheenwrites@gmail.com>
2026-03-09 15:49:42 +0500

    Merge About section

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ git init
 git config user.name "Maheen Nouman"
 git config user.email "imaheenwrites@gmail.com"
 git add .
-git commit -m "Update portfolio with SEO and image fixes"
+git commit -m "Merge About section into HomePage"
 git branch -M main
 git remote add origin https://github.com/usamakswati/Portfolio-MaheensPalette.git
 git remote set-url origin https://github.com/usamakswati/Portfolio-MaheensPalette.git
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 export default function HomePage() {
-    return <Hero />;
+    return (
+        <>
+            <Hero />
+            <About />
+        </>
+    );
 }
